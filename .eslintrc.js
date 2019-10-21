04776c5cdac27fa2fcdb2704254f6b688af91ac8
Bruno Santos <bruno.santos@pottencial.com.br>
2019-10-21 11:24:52 -0300

    Star match

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,6 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     indent: ['error', 2, { SwitchCase: 1 }],
     'comma-dangle': ['error', 'always-multiline'],
-    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-console': ['warn', { allow: ['info', 'error'] }],
